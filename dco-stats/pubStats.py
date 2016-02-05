9a4f5b66ac0d8065158e683f5b2ce8347c907b7d
mrpatrickwest <westp@rpi.edu>
2016-02-05 01:07:24 -0500

    Displaying publications in citation formation
    Grabbing the volume, issue, start page and end page in order to display
    publications correctly.

--- dco-stats/pubStats.py
+++ dco-stats/pubStats.py
@@ -11,25 +11,37 @@ teamlist = {}
 
 def printIt( uri, jObj ):
     if jObj and len( jObj ) > 0 and "title" in jObj[0]:
-        print( jObj[0]["title"]["value"] )
-        if "dcoId" in jObj[0]:
-            print( "    DCO-ID: " + jObj[0]["dcoId"]["value"] )
-        print( "    Publication Type: " + jObj[0]["type"]["value"] )
+        print( "<br/>" )
         if "contrib" in jObj[0]:
-            print( "    Is Contribution to the DCO: " + jObj[0]["contrib"]["value"] )
+            print( "Is Contribution to the DCO: " + jObj[0]["contrib"]["value"] + "<br/>"  )
         else:
-            print( "    Is Contribution to the DCO: No" ) ;
-        if "year" in jObj[0]:
-            print( "    Publication Year: " + jObj[0]["year"]["value"] )
-        if "doi" in jObj[0]:
-            print( "    DOI: " + jObj[0]["doi"]["value"] )
-        if "journal" in jObj[0]:
-            print( "    Journal: " + jObj[0]["journal"]["value"] )
+            print( "Is Contribution to the DCO: No" + "<br/>"  )
+        #if "dcoId" in jObj[0]:
+        #    print( "    DCO-ID: " + jObj[0]["dcoId"]["value"] + "<br/>"  )
+        print( "    ", end=" " )
         if "Authors" in jObj[0]:
-            print( "    Authors: " + jObj[0]["Authors"]["value"] )
+            print( jObj[0]["Authors"]["value"].replace(";",","), end=" " )
+        if "year" in jObj[0]:
+            print( "(" + jObj[0]["year"]["value"] + ")", end=" " )
+        if "doi" in jObj[0]:
+            print( "<a href=\"http://dx.doi.org/" + jObj[0]["doi"]["value"] + "\">", end=" " )
+            print( jObj[0]["title"]["value"], end=" " )
+            print( "</a>.", end=" " )
+        else:
+            print( jObj[0]["title"]["value"] + ".", end=" " )
+        if "journal" in jObj[0]:
+            print( jObj[0]["journal"]["value"], end=" " )
+            if "volume" in jObj[0]:
+                print( jObj[0]["volume"]["value"], end="" )
+            if "issue" in jObj[0]:
+                print( "(" + jObj[0]["issue"]["value"] + ")", end="" )
+            if "pagestart" in jObj[0]:
+                print( ":" + jObj[0]["pagestart"]["value"] + "-", end="" )
+            if "pageend" in jObj[0]:
+                print( jObj[0]["pageend"]["value"], end="" )
+        print( "<br/>" )
         if "Communities" in jObj[0]:
             comms = jObj[0]["Communities"]["value"]
-            print( "    Communities: " + comms )
             for comm in comms.split(';'):
                 if comm in communities:
                     communities[comm]+=1
@@ -37,7 +49,6 @@ def printIt( uri, jObj ):
                     communities[comm] = 1
         if "Teams" in jObj[0]:
             teams = jObj[0]["Teams"]["value"]
-            print( "    Teams: " + teams )
             for team in teams.split(';'):
                 if team in teamlist:
                     teamlist[comm]+=1
